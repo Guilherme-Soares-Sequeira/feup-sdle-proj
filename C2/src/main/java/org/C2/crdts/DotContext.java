4b05270386db78e6d7a7d17483779a70a7f6b1f9
Francisco Prada <fmamp24@gmail.com>
2023-12-01 17:18:45 +0000

    dotcontext done

--- C2/src/main/java/org/C2/crdts/DotContext.java
+++ C2/src/main/java/org/C2/crdts/DotContext.java
@@ -1,53 +1,86 @@
 package org.C2.crdts;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import org.C2.utils.Pair;
+import java.util.*;
 
-public class DotContext<K> {
-    private final Map<K, Integer> cc = new HashMap<>(); // Compact causal context
-    private final Set<Pair<K, Integer>> dc = new HashSet<>(); // Dot cloud
 
-    public DotContext<K> copy() {
-        DotContext<K> copy = new DotContext<>();
-        copy.cc.putAll(this.cc);
-        copy.dc.addAll(this.dc);
-        return copy;
+
+public class DotContext {
+    private Map<String, Integer> causalContext; // Compact causal context
+    private Set<Dot> dotCloud; // Dot cloud
+
+    public DotContext() {
+        this.causalContext = new HashMap<>();
+        this.dotCloud = new HashSet<>();
+    }
+
+
+    public boolean containsDot(Dot dot) {
+        Integer itm = this.causalContext.get(dot.getReplicaID());
+        if (itm != null && dot.getSequenceNumber() <= itm) return true;
+        return dotCloud.contains(dot);
     }
 
     public void compact() {
-        boolean flag;
-        do {
+        boolean flag = true;
+        while(flag) {
             flag = false;
-            Set<Pair<K, Integer>> toRemove = new HashSet<>();
-            for (Pair<K, Integer> dot : dc) {
-                Integer ccValue = cc.get(dot.getFirst());
-                if (ccValue == null) {
-                    if (dot.getSecond() == 1) {
-                        cc.put(dot.getFirst(), dot.getSecond());
-                        toRemove.add(dot);
+            for (Dot dot : this.dotCloud) {
+                Integer sequenceNumber = this.causalContext.get(dot.getReplicaID());
+                if (sequenceNumber == null) {
+                    if (dot.getSequenceNumber() == 1) {
+                        this.causalContext.put(dot.getReplicaID(), dot.getSequenceNumber());
+                        this.dotCloud.remove(dot);
                         flag = true;
                     }
-                } else if (dot.getSecond() == ccValue + 1) {
-                    cc.put(dot.getFirst(), ccValue + 1);
-                    toRemove.add(dot);
-                    flag = true;
-                } else if (dot.getSecond() <= ccValue) {
-                    toRemove.add(dot);
+                } else {
+                    if (dot.getSequenceNumber() == this.causalContext.get(dot.getReplicaID()) + 1) {
+                        this.causalContext.put(dot.getReplicaID(), dot.getSequenceNumber() + 1);
+                        this.dotCloud.remove(dot);
+                        flag = true;
+                    } else {
+                        if (dot.getSequenceNumber() <= this.causalContext.get(dot.getReplicaID())) {
+                            this.dotCloud.remove(dot);
+                        }
+                    }
                 }
             }
-            dc.removeAll(toRemove);
-        } while (flag);
+        }
     }
 
-    public void join(DotContext<K> o) {
-        for (Map.Entry<K, Integer> entry : o.cc.entrySet()) {
-            cc.merge(entry.getKey(), entry.getValue(), Integer::max);
+
+    public Dot makeDot(String id) {
+        Integer existing = causalContext.get(id);
+        if (existing != null) {
+            causalContext.put(id, existing + 1);
+            return new Dot(id, existing + 1);
+        } else {
+            causalContext.put(id, 1);
+            return new Dot(id, 1);
+        }
+    }
+
+    public void insertDot(Dot d, boolean compactNow) {
+        dotCloud.add(d);
+        if (compactNow) compact();
+    }
+
+    public void join(DotContext o) {
+        if (o == this) return;
+
+        for (Map.Entry<String, Integer> entry: o.causalContext.entrySet()){
+            Integer selfSequenceNumber = this.causalContext.get(entry.getKey());
+            if (selfSequenceNumber == null){
+                this.causalContext.put(entry.getKey(), entry.getValue());
+            }
+            else{
+                this.causalContext.put(entry.getKey(), entry.getValue());
+            }
+        }
+        for(Dot dot : o.dotCloud) {
+            this.insertDot(dot, false);
         }
 
-        dc.addAll(o.dc);
-        compact();
-    }
+        this.compact();
 
+    }
+    
 }

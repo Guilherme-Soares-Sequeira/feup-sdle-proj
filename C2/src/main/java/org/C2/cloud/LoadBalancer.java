37baf25828df95639b09f2d1738726ab101ca348
Francisco Prada <fmamp24@gmail.com>
2023-12-10 00:12:52 +0000

    added remaining endpoints to load balancer

--- C2/src/main/java/org/C2/cloud/LoadBalancer.java
+++ C2/src/main/java/org/C2/cloud/LoadBalancer.java
@@ -1,6 +1,7 @@
 package org.C2.cloud;
 import org.C2.utils.*;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 import spark.Request;
 import spark.Response;
 
@@ -33,7 +34,203 @@ public class LoadBalancer extends BaseServer {
     protected void defineRoutes() {
         get("/read/:id", this::read);
         put("/write/:id", this::write);
+        put("/nodes/read/", this::nodeRequestRead);
+        put("/nodes/write/", this::nodeRequestWrite);
+        get("/client/pull/:id", this::clientRequestPull);
+        get("/client/read/:id", this::clientRequestRead);
     }
+
+    private String nodeRequestWrite(Request req, Response res) {
+        final JSONObject response = new JSONObject();
+        final String endpoint = "PUT /nodes/";
+
+        JSONObject bodyJson;
+
+        try{
+            bodyJson = new JSONObject(new JSONTokener(req.body()));
+        } catch (Exception e) {
+            final String errorString = "Could not parse body as JSON";
+
+            this.logError(endpoint, errorString);
+            res.status(400);
+            response.put(JsonKeys.errorMessage, errorString);
+
+            return response.toString();
+        }
+
+        String forID;
+        try {
+            forID = bodyJson.getString(JsonKeys.forId);
+        } catch (Exception e) {
+            final String errorString = "Could not get forID from request";
+
+            this.logError(endpoint, errorString);
+            res.status(400);
+            response.put(JsonKeys.errorMessage, errorString);
+
+            return response.toString();
+        }
+
+        RequestStatus status;
+        try {
+            status = this.requestStatusMap.get(forID);
+        } catch (Exception e) {
+            final String errorString = "Could not get status from request";
+
+            this.logError(endpoint, errorString);
+            res.status(400);
+            response.put(JsonKeys.errorMessage, errorString);
+
+            return response.toString();
+        }
+
+
+        if (forID == null || status == null) {
+            final String errorString = "Incomplete node request parameters";
+
+            this.logError(endpoint, errorString);
+            res.status(400);
+            response.put(JsonKeys.errorMessage, errorString);
+
+            return response.toString();
+        }
+
+        requestStatusMap.put(forID, status);
+
+        res.status(200);
+        return "";
+    }
+
+    private String nodeRequestRead(Request req, Response res){
+        final JSONObject response = new JSONObject();
+        final String endpoint = "PUT /nodes/";
+
+        JSONObject bodyJson;
+
+        try{
+            bodyJson = new JSONObject(new JSONTokener(req.body()));
+        } catch (Exception e) {
+            final String errorString = "Could not parse body as JSON";
+
+            this.logError(endpoint, errorString);
+            res.status(400);
+            response.put(JsonKeys.errorMessage, errorString);
+
+            return response.toString();
+        }
+
+        String forID;
+        try {
+            forID = bodyJson.getString(JsonKeys.forId);
+        } catch (Exception e) {
+            final String errorString = "Could not get forID from request";
+
+            this.logError(endpoint, errorString);
+            res.status(400);
+            response.put(JsonKeys.errorMessage, errorString);
+
+            return response.toString();
+        }
+
+        RequestStatus status;
+        try {
+            status = this.requestStatusMap.get(forID);
+        } catch (Exception e) {
+            final String errorString = "Could not get status from request";
+
+            this.logError(endpoint, errorString);
+            res.status(400);
+            response.put(JsonKeys.errorMessage, errorString);
+
+            return response.toString();
+        }
+
+        String list;
+        try {
+            list = bodyJson.getString(JsonKeys.list);
+        } catch (Exception e) {
+            final String errorString = "Could not get list from request";
+
+            this.logError(endpoint, errorString);
+            res.status(400);
+            response.put(JsonKeys.errorMessage, errorString);
+
+            return response.toString();
+        }
+
+        if (forID == null || status == null || list == null) {
+            final String errorString = "Incomplete node request parameters";
+
+            this.logError(endpoint, errorString);
+            res.status(400);
+            response.put(JsonKeys.errorMessage, errorString);
+
+            return response.toString();
+        }
+
+        requestStatusMap.put(forID, status);
+        requestedLists.put(forID, list);
+
+        res.status(200);
+        return "";
+    }
+
+
+
+    private String clientRequestPull(Request req, Response res) {
+        final JSONObject response = new JSONObject();
+        final String endpoint = "GET /client/{ID}";
+
+        String forID = req.params(":id");
+
+        if (forID == null) {
+            final String errorString = "Could not get id from client request";
+
+            this.logError(endpoint, errorString);
+            res.status(400);
+            response.put(JsonKeys.errorMessage, errorString);
+
+            return response.toString();
+        }
+
+        RequestStatus status = requestStatusMap.getOrDefault(forID, RequestStatus.ERROR);
+
+        response.put("status", status.toString());
+
+        res.status(200);
+        return "";
+    }
+
+
+    public String clientRequestRead(Request req, Response res) {
+        final JSONObject response = new JSONObject();
+        final String endpoint = "GET /client/read/{ID}";
+
+        String forID = req.params(":id");
+
+        if (forID == null) {
+            final String errorString = "Could not get id from client request";
+
+            this.logError(endpoint, errorString);
+            res.status(400);
+            response.put(JsonKeys.errorMessage, errorString);
+
+            return response.toString();
+        }
+
+        RequestStatus status = requestStatusMap.getOrDefault(forID, RequestStatus.ERROR);
+
+        if (status == RequestStatus.DONE) {
+            String list = requestedLists.get(forID);
+            response.put(JsonKeys.list, list);
+        }
+
+        response.put("status", status.toString());
+
+        res.status(200);
+        return response.toString();
+    }
+
 
     private ServerInfo getRandomServer() {
         //get all servers
